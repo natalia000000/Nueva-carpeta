a2a499984ddddcf258ceb452b6b78555a0a23f0e
natalia000000 <152176051+natalia000000@users.noreply.github.com>
2024-07-02 11:55:52 -0400

    Add files via upload

---
+++ Practica_pruebas.py
@@ -0,0 +1,7 @@
+def introduction(first_name, last_name="Smith"):
+    print("Hola, mi nombre es", first_name, last_name)
+introduction("Jorge", "Pérez")
+introduction("Enrique")
+introduction(first_name="Guillermo")
+
+
